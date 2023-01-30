d315e62c998d6cc82683558dafc859d628788146
Nitsua28 <achan113@ucmerced.edu>
2023-01-29 18:26:56 -0800

    added hp bar and damage splat

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
+// $(document).ready(function() {
+//     var current_progress = 0;
+//     var interval = setInterval(function() {
+//         current_progress += 10;
+//         $("#dynamic")
+//         .css("width", current_progress + "%")
+//         .attr("aria-valuenow", current_progress)
+//         .text(current_progress + "% Complete");
+//         if (current_progress >= 100)
+//             clearInterval(interval);
+//     }, 1000);
+//   });
 
 let weaponInfo = {};
 let bossInfo = {};
 
 let bossName = "";
 let wepName = "";
+
+let bossMaxHp;
+let bossCurrentHp;
+let damage;
 
 function setBossName(event = new Event()){
     bossName = event.target.value;
@@ -12,7 +28,9 @@ function setBossName(event = new Event()){
 function setWeaponName(event = new Event()){
     wepName = event.target.value;
 }
+
 async function generateInfo(){
+    reset();
     const httpResponseBoss = await fetch("https://eldenring.fanapis.com/api/bosses?name=" + bossName);
     bossInfo = await httpResponseBoss.json();
 
@@ -22,7 +40,19 @@ async function generateInfo(){
     console.log(weaponInfo)
     renderInfo();
 }
+function reset(){
 
+    const weapon_name_container = document.getElementsByClassName("weaponname-container")[0];
+    const bossname_container = document.getElementsByClassName("bossname-container")[0];
+    const damage_splat_container = document.getElementsByClassName("bossname-container")[0];
+
+    if (weapon_name_container.firstChild !== null)
+        weapon_name_container.removeChild(weapon_name_container.firstChild);
+    if (bossname_container.firstChild !== null)
+        bossname_container.removeChild(bossname_container.firstChild);
+    if (damage_splat_container.firstChild !== null)
+        damage_splat_container.removeChild(damage_splat_container.firstChild);
+}
 function renderInfo(){
     const bossname = bossInfo.data[0].name
     const bosspicture = bossInfo.data[0].image
@@ -30,9 +60,10 @@ function renderInfo(){
 
     const weaponname = weaponInfo.data[0].name
     const weaponpicture = weaponInfo.data[0].image
-    const damage = weaponInfo.data[0].attack[0].amount;
+    damage = weaponInfo.data[0].attack[0].amount;
 
     const hp_bar_container = document.getElementsByClassName("hpbar-container")[0];
+    
     const bossname_container = document.getElementsByClassName("bossname-container")[0];
     const bossimage_container = document.getElementsByClassName("bossimage-container")[0];
     
@@ -44,11 +75,62 @@ function renderInfo(){
     console.log(bossname)
     bossimage.setAttribute("src", bosspicture);
     
-    hp_bar_container.append(healthPoints);
+    //hp_bar_container.append(healthPoints);
+    const cleanHealthPoints = healthPoints.replace(/\D/g, "");
+    cleanHealthPoints === "" ? 
+    bossMaxHp = bossCurrentHp = 1000 :  
+    bossMaxHp = bossCurrentHp = parseInt(cleanHealthPoints);
+    
+    setHpBarInfo(bossCurrentHp,bossMaxHp,bossCurrentHp.toString());
 
     weapon_name_container.append(weaponname);
     weaponpic.setAttribute("src", weaponpicture);
     damage_splat_container.append(damage);
 
 
+}
+
+
+// $(document).ready(function() {
+//     var current_progress = 0;
+//     var interval = setInterval(function() {
+//         current_progress += 10;
+//         $("#dynamic")
+//         .css("width", current_progress + "%")
+//         .attr("aria-valuenow", current_progress)
+//         .text(current_progress + "% Complete");
+//         if (current_progress >= 100)
+//             clearInterval(interval);
+//     }, 1000);
+//   });
+
+function whack(){
+    addDamageSplat()
+    bossCurrentHp = bossCurrentHp - damage;
+
+    if (bossCurrentHp > 0){
+        setHpBarInfo(bossCurrentHp,bossMaxHp,bossCurrentHp.toString());
+    }
+    else{
+        setHpBarInfo(0,bossMaxHp);
+    }
+}
+
+function addDamageSplat(){
+    const container = document.getElementsByClassName("damage-splat-container")[0]
+    const splat = document.createElement("div");
+    if (container)
+    splat.setAttribute("id", "damage-splat");
+    splat.innerHTML = damage.toString();
+    container.appendChild(splat);
+    
+}
+function setHpBarInfo(currentHp,maxHp,text = 'DEAD'){
+    const hpbar = document.getElementById("dynamic");
+    const hpbarText = document.getElementById('current-progress');
+    hpbar.style.width = (currentHp / maxHp) * 100 + "%";
+    hpbar.setAttribute("aria-valuemax", maxHp);
+    hpbar.setAttribute("aria-valuenow", currentHp);
+    hpbarText.innerHTML = text;
+    console.log(text)
 }
